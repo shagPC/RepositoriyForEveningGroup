06fa51cc02e138ae1b9ae3d618ff9c989419bfae
Квест <@410-TEACHER>
2019-02-25 19:43:45 +0600

    changes 2

--- UnitTestsForProject/UnitTestsForProject/UnitTest1.cs
+++ UnitTestsForProject/UnitTestsForProject/UnitTest1.cs
@@ -9,7 +9,7 @@ namespace UnitTestsForProject
         [TestMethod]
         public void TestMethod1()
         {
-            Console.WriteLine("Hello?");
+            Console.WriteLine("Hello???");
             Console.WriteLine("Привет?");
         }
     }
