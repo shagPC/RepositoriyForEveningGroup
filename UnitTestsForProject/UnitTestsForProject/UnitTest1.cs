f77bbed9e8cfa100a30706548f0e2a775e0dfe02
ЗатовД <@DESKTOP-9F85AP5>
2019-02-25 20:04:57 +0600

    changes Add  Console.WriteLine("МОИ ИЗМЕНЕНИЯ ТАКОВЫ!");

--- UnitTestsForProject/UnitTestsForProject/UnitTest1.cs
+++ UnitTestsForProject/UnitTestsForProject/UnitTest1.cs
@@ -9,8 +9,7 @@ namespace UnitTestsForProject
         [TestMethod]
         public void TestMethod1()
         {
-            Console.WriteLine("Hello");
-            Console.WriteLine("Hi");
+            Console.WriteLine("МОИ ИЗМЕНЕНИЯ ТАКОВЫ!");
         }
     }
 }
