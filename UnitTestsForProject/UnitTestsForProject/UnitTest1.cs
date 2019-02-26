aa1f6782ecc6f8d637a36a6f4093ff90791ee3bd
ЗатовД <@DESKTOP-9F85AP5>
2019-02-26 19:02:35 +0600

    add : Console.WriteLine("Изменяю development");

--- UnitTestsForProject/UnitTestsForProject/UnitTest1.cs
+++ UnitTestsForProject/UnitTestsForProject/UnitTest1.cs
@@ -9,7 +9,7 @@ namespace UnitTestsForProject
         [TestMethod]
         public void TestMethod1()
         {
-            Console.WriteLine("Изменяю dev");
+            Console.WriteLine("Изменяю development");
         }
     }
 }
