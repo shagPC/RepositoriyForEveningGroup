77463a113bc38b233eefa4bcc8c2411ca80c324e
ЗатовД <@DESKTOP-9F85AP5>
2019-02-26 19:12:00 +0600

    Add class "Daryn"

---
+++ UnitTestsForProject/UnitTestsForProject/Daryn.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestsForProject
+{
+    public class Daryn
+    {
+        string Name { get; set; }
+        int Age { get; set; }
+        private Daryn()
+        {
+            Name = "Daryn";
+            Age = 16;
+        }
+        static Daryn daryn;
+        public static Daryn GetDaryn()
+        {
+            if (daryn == null)
+                daryn = new Daryn();
+            return daryn;
+        }
+        public void InformationAboutDaryn()
+        {
+            Console.WriteLine($"{Name} - {Age}");
+        }
+    }
+}
